932bf1d7c31ca12e9cc06aa196d6aa1baf4edce9
suhail7868 <suhail98ever@gmail.com>
2023-12-21 00:38:30 +0530

    commit 2

--- home/views_api.py
+++ home/views_api.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
+from .models import Profile
+from .helper import *
 from django.contrib.auth import authenticate, login
 
 
